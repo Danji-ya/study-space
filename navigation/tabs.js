5f3d6c929c03d98be960d59fe44301629709fec0
Danji-ya <erangerang15@gmail.com>
2021-03-12 16:45:32 +0900

    home ing...

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -2,17 +2,78 @@ import React from 'react';
 import {
     View,
     Image,
-    TouchableOpacity
+    TouchableOpacity,
+    Touchable
 } from 'react-native';
 
 import { createBottomTabNavigator, BottomTabBar } from '@react-navigation/bottom-tabs';
 
+import Svg, { Path } from 'react-native-svg';
 
 import {Home} from '../screens';
 
 import { COLORS, icons } from '../constants';
 
 const Tab = createBottomTabNavigator();
+
+//하단 tabBar customalize
+const TabBarCustomButton = ({ accessibilityState, children, onPress }) => {
+    let isSelected = accessibilityState.selected;
+
+    if (isSelected) {
+        return (
+            <View style={{ flex:1, alignItems: 'center'}}>
+                <View style={{ flexDirection: 'row', position: 'absolute', top: 0}}>
+                    <View style={{ flex:1, backgroundColor: COLORS.white}}></View>
+                    <Svg
+                        width={70}
+                        height={60}
+                        viewBox="0 0 75 61"
+                    >
+                        <Path
+                            d="M75.2 0v61H0V0c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1h-.1z"
+                            fill={COLORS.white}
+                        />
+                    </Svg>
+                    <View style={{ flex: 1, backgroundColor: COLORS.white}}></View>
+                </View>
+
+                <TouchableOpacity
+                    style= {{
+                        top: -20,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        width: 50,
+                        height: 50,
+                        borderRadius: 25,
+                        backgroundColor: COLORS.white,
+            
+                    }}
+                    onPress={onPress}
+                >
+                {children}
+                </TouchableOpacity>
+            </View>
+
+        )
+    } else {
+        return (      
+            <TouchableOpacity
+                style= {{
+                    flex: 1,
+                    height: 60,
+                    backgroundColor: COLORS.white,
+        
+                }}
+                activeOpacity={1}
+                onPress={onPress}
+            >
+            {children}
+            </TouchableOpacity>
+
+        )
+    }
+}
 
 const Tabs = () => {
     return (
@@ -40,6 +101,9 @@ const Tabs = () => {
                                 tintColor: focused ? COLORS.primary : COLORS.secondary,
                             }}
                         />
+                    ),
+                    tabBarButton: (props) => (
+                        <TabBarCustomButton {...props} />
                     )
                 }}
             />
@@ -57,6 +121,9 @@ const Tabs = () => {
                                 tintColor: focused ? COLORS.primary : COLORS.secondary,
                             }}
                         />
+                    ),
+                    tabBarButton: (props) => (
+                        <TabBarCustomButton {...props} />
                     )
                 }}
             />
@@ -74,6 +141,9 @@ const Tabs = () => {
                                 tintColor: focused ? COLORS.primary : COLORS.secondary,
                             }}
                         />
+                    ),
+                    tabBarButton: (props) => (
+                        <TabBarCustomButton {...props} />
                     )
                 }}
             />
@@ -91,6 +161,9 @@ const Tabs = () => {
                                 tintColor: focused ? COLORS.primary : COLORS.secondary,
                             }}
                         />
+                    ),
+                    tabBarButton: (props) => (
+                        <TabBarCustomButton {...props} />
                     )
                 }}
             />
@@ -101,3 +174,4 @@ const Tabs = () => {
 
 
 export default Tabs;
+
