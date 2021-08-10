7767240274c568be347f0a07afea1368c3963688
Danji-ya <erangerang15@gmail.com>
2021-08-10 14:15:31 +0900

    feat: 각 이벤트에 대한 핸들러 구현

--- calculator.js
+++ calculator.js
@@ -1,31 +1,85 @@
-const calculatorDisplay = document.querySelector('.calcalator-display');
+const calculatorDisplay = document.querySelector('.calcalator-display > p');
 const buttons = document.querySelectorAll('.calcalator-buttons > button');
 
-const operator = e => {
-    // innerText와 innerHTML 차이 블로그에 적어두기
-    console.log(e.currentTarget.innerText);
-};
-const getValue = e => {
-    console.log(e.currentTarget.value);
-};
-
-const decimal = e => {
-    console.log(e.currentTarget);
+const operators = {
+    '+': (value1, value2) => value1 + value2,
+    '-': (value1, value2) => value1 - value2,
+    x: (value1, value2) => value1 * value2,
+    '÷': (value1, value2) => value1 / value2,
+    '=': (value1 = null, value2) => value2,
 };
 
-const clearAll = e => {
-    console.log(e.currentTarget);
+let waitingValue = false;
+let prevValue = '';
+let prevOperator = '';
+
+const getNextValue = operator => {
+    waitingValue = true;
+    prevOperator = operator;
 };
 
-[...buttons].map(button => {
-    // classList 사용한 이유: contains 이용하기 위해서 className에서는 x
-    if (button.classList.contains('operator')) {
-        button.addEventListener('click', operator);
-    } else if (button.classList.contains('decimal')) {
-        button.addEventListener('click', decimal);
-    } else if (button.classList.contains('init')) {
-        button.addEventListener('click', clearAll);
-    } else {
-        button.addEventListener('click', getValue);
+const numberOperation = e => {
+    // first operator
+    if (!prevOperator) return getNextValue(e.currentTarget.innerText);
+
+    // from the second operator && operator's turn
+    if (!waitingValue) {
+        calculatorDisplay.innerText = operators[prevOperator](
+            Number(prevValue),
+            Number(calculatorDisplay.innerText),
+        );
     }
-});
+
+    return getNextValue(e.currentTarget.innerText);
+};
+
+const getValue = e => {
+    const displayValue = calculatorDisplay.innerText;
+
+    if (waitingValue) {
+        prevValue = displayValue;
+        calculatorDisplay.innerText = e.currentTarget.value;
+
+        waitingValue = false;
+    } else {
+        calculatorDisplay.innerText =
+            displayValue === '0'
+                ? e.currentTarget.value
+                : displayValue + e.currentTarget.value;
+    }
+};
+
+const decimal = () => {
+    const displayValue = calculatorDisplay.innerText;
+    if (!displayValue.includes('.')) {
+        calculatorDisplay.innerText = `${displayValue}.`;
+    }
+};
+
+const clearAll = () => {
+    waitingValue = false;
+    prevValue = '';
+    prevOperator = '';
+    calculatorDisplay.innerText = '0';
+};
+
+/* ------------------------------ DOM control------------------------------------------------ */
+const addListener = () => {
+    [...buttons].map(button => {
+        if (button.classList.contains('operator')) {
+            button.addEventListener('click', numberOperation);
+        } else if (button.classList.contains('decimal')) {
+            button.addEventListener('click', decimal);
+        } else if (button.classList.contains('init')) {
+            button.addEventListener('click', clearAll);
+        } else {
+            button.addEventListener('click', getValue);
+        }
+    });
+};
+
+const init = () => {
+    addListener();
+};
+
+init();
