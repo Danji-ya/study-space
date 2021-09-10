71bae5b9711f72ba0d00947a0da99c8496d1d4a8
Danji-ya <erangerang15@gmail.com>
2021-09-10 19:53:01 +0900

    feat: 일정 div 범위 내에서 요소 fixed 구현

--- app/js/third.js
+++ app/js/third.js
@@ -1,3 +1,41 @@
 // default
 const header = document.querySelector('header');
+// const descriptionWrap = document.querySelector('.description-wrap');
+// const feeWrap = document.querySelector('.fee-desc-wrap');
+
+// const feeWrapWidth = feeWrap.offsetWidth;
+// const feeWrapHeight = feeWrap.offsetHeight;
 header.classList.add('sticky');
+
+
+// function checkMaxScroll() {
+
+//     const offsetBottom = descriptionWrap.offsetTop + descriptionWrap.offsetHeight;
+
+//     const isMax = () => offsetBottom < window.scrollY + feeWrap.offsetHeight + feeWrap.offsetTop;
+
+//     if(isMax()) {
+//         descriptionWrap.classList.remove('fixed');
+//         descriptionWrap.classList.add('end');
+//     } else {
+//         descriptionWrap.classList.remove('end');
+//     }
+// }
+
+
+// function fixedFeeWrap() {
+
+
+//     if(window.scrollY > descriptionWrap.offsetTop){
+
+//         checkMaxScroll();
+
+//         descriptionWrap.classList.add('fixed');
+//         feeWrap.style.width = `${feeWrapWidth}px`;
+//         // feeWrap.style.height = `${feeWrapHeight}px`;
+//     } else {
+//         descriptionWrap.classList.remove('fixed');
+//     }
+// }
+
+// window.addEventListener('scroll', fixedFeeWrap);
