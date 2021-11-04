039495d2ea61f9bbd3723748773b53ec9d5a1aef
Danji-ya <erangerang15@gmail.com>
2021-11-04 14:53:16 +0900

    file-loader와 url-loader 적용(webpack5)

--- src/app.js
+++ src/app.js
@@ -1,4 +1,10 @@
 import * as math from './math.js';
 import './style.css';
+import nyancat from './nyancat.jpg';
 
 console.log(math.sum(1, 2));
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.innerHTML = `<img src="${nyancat}" />`;
+});
--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,24 @@ module.exports = {
   output: {
     filename: "[name].js",
     path: path.resolve("./dist"),
+    assetModuleFilename: "[name][ext]?[hash]"
   },
   module: {
-    rules: [{
-      test: /\.css$/, // css 확장자로 끝나는 모든 파일을 의미
-      use: ["style-loader","css-loader"], // style-loader 및 css-loader 로더 적용
-    }],
+    rules: [
+      {
+        test: /\.css$/, // css 확장자로 끝나는 모든 파일을 의미
+        use: ["style-loader","css-loader"], // style-loader 및 css-loader 적용
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        // type: "asset/resource" // file-loader와 같은 효과
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 20 * 1024, // 기준으로 20kb 로 변경
+          },
+        }  
+      }
+    ],
   }
 }
