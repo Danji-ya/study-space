eae2758a312571053d410178f59d947dedbea9e2
Danji-ya <erangerang15@gmail.com>
2021-11-08 18:02:44 +0900

    babel과 웹팩 통합 이전

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-const alert = msg => window.alert(msg);
+new Promise();
--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,15 @@
 // babel.config.js:
 module.exports = {
-    presets: ["./mypreset.js"],
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          useBuiltIns: "usage", // 폴리필 사용 방식 지정
+          corejs: {
+            // 폴리필 버전 지정
+            version: 3,
+          },
+        },
+      ],
+    ],
 }
