589065d6a691bd80892bf697bfe1baf333c457bc
Danji-ya <erangerang15@gmail.com>
2021-12-05 12:34:08 +0900

    그리디 문제풀이2

--- etc/41/41.js
+++ etc/41/41.js
@@ -1,36 +1,52 @@
-function solution(n, k, data) {
-    let [turnOn, turnOff] = [data[0], data[0] + 1];
-    let termArr = [];
-    let termCnt = 1;
-    let answer = 0;
+function solution(n, k , data) {    
+  const diffList = [];
+  let answer = n;
   
-    for (let i = 1; i < data.length; i += 1) {
-      let [nextTurnOn, nextTurnOff] = [data[i], data[i] + 1];
+  for(let i=0; i<data.length-1; i+=1){
+      diffList.push(data[i+1] - data[i] -1);
+  }
   
-      // 연속적이지 않을 때,
-      if (turnOff < nextTurnOn) {
-        termArr.push(nextTurnOn - turnOff);
+  diffList.sort((a,b) => a-b);
   
-        termCnt += 1;
-      }
+  for(let i=0; i<n-k; i+=1){
+      answer+=diffList[i];
+  }
   
-      turnOn = nextTurnOn;
-      turnOff = nextTurnOff;
-    }
-  
-    // 오름차순
-    termArr = termArr.sort((a, b) => a - b);
-  
-    // termCnt <= k 그냥 n
-    // 아니면 텀이 가장 짧은 구간을 성냥을 아끼고 긴 구간에서 성냥쓰기
-    if (termCnt > k) {
-      for (let i = 0; i < termCnt - k; i += 1) {
-        answer += termArr[i];
-      }
-    }
-  
-    console.log(answer + n);
+  console.log(answer);
 }
+// function solution(n, k, data) {
+//     let [turnOn, turnOff] = [data[0], data[0] + 1];
+//     let termArr = [];
+//     let termCnt = 1;
+//     let answer = 0;
+  
+//     for (let i = 1; i < data.length; i += 1) {
+//       let [nextTurnOn, nextTurnOff] = [data[i], data[i] + 1];
+  
+//       // 연속적이지 않을 때,
+//       if (turnOff < nextTurnOn) {
+//         termArr.push(nextTurnOn - turnOff);
+  
+//         termCnt += 1;
+//       }
+  
+//       turnOn = nextTurnOn;
+//       turnOff = nextTurnOff;
+//     }
+  
+//     // 오름차순
+//     termArr = termArr.sort((a, b) => a - b);
+  
+//     // termCnt <= k 그냥 n
+//     // 아니면 텀이 가장 짧은 구간을 성냥을 아끼고 긴 구간에서 성냥쓰기
+//     if (termCnt > k) {
+//       for (let i = 0; i < termCnt - k; i += 1) {
+//         answer += termArr[i];
+//       }
+//     }
+  
+//     console.log(answer + n);
+// }
   
 const readline = require("readline");
 const rl = readline.createInterface({
