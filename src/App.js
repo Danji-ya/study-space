b5cc117d0dc0d2c795c4ce3c25129bbd22085be8
Danji-ya <erangerang15@gmail.com>
2022-01-18 17:34:53 +0900

    test 실패를 위한 내용추가

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          실패를 위한 리액트 Learn React
         </a>
       </header>
     </div>
