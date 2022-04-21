e9d4b937c733c4b82d0e1e6b5dd5807afb13a751
Danji-ya <erangerang15@gmail.com>
2022-04-21 11:47:07 +0900

    [test] 새로운 google login sdk, react의 shallow compare

---
+++ shallowcompare/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// render twice 잠시 제거
+
+root.render(
+  // <React.StrictMode>
+    <App />
+  // </React.StrictMode>
+);
