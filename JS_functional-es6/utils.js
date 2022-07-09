5bd100b795c88a3eef819d3e5705fd9245889f5c
Danji-ya <erangerang15@gmail.com>
2022-07-09 14:07:36 +0900

    [feat]: C 유틸함수로 추가

--- JS_functional-es6/utils.js
+++ JS_functional-es6/utils.js
@@ -145,6 +145,21 @@ const find = (f, iter) => go(
   take(1),
   ([a]) => a);
 
+// ------- 병렬적 비동기 --------
+function noop() {}
+const catchNoop = ([...arr]) => 
+  (arr.forEach(a => a instanceof Promise ? a.catch(noop) : a), arr);
+
+const C = {};
+C.reduce = curry((f, acc, iter) => iter ? 
+  reduce(f, acc, catchNoop(iter)) :
+  reduce(f, catchNoop(acc))
+);
+C.take = curry((l, iter) => take(l, catchNoop(iter)));
+C.takeAll = C.take(Infinity);
+C.map = curry(pipe(L.map, C.takeAll));
+C.filter = curry(pipe(L.filter, C.takeAll));
+
 module.exports = {
   curry,
   map,
@@ -157,5 +172,6 @@ module.exports = {
   L,
   flatten,
   flatMap,
-  find
+  find,
+  C
 }
