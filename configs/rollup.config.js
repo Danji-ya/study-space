4cbd9d41a1e509ae995329eb44d58bc3564eb429
Danji-ya <erangerang15@gmail.com>
2023-01-07 16:47:36 +0900

    chore: build external 추가

--- configs/rollup.config.js
+++ configs/rollup.config.js
@@ -1,19 +1,27 @@
 import fs from 'fs';
+import path from 'path';
 import readPkgUp from 'read-pkg-up';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import commonjs from '@rollup/plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
 
-const excludePath = 'node_modules/**';
-
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
-const { packageJson: pkg } = readPkgUp.sync({
-  cwd: fs.realpathSync(process.cwd()),
+const { packageJson } = readPkgUp.sync({
+	cwd: fs.realpathSync(process.cwd()),
 });
+
+const getExternals = (pkg) => {
+	const defaultExternal = [/@babel\/runtime/];
+	const pkgDependencies = Object.keys(pkg.dependencies || {});
+	const pkgPeerDependencies = Object.keys(pkg.peerDependencies || {});
+	
+	const externals = [...defaultExternal, ...pkgDependencies, ...pkgPeerDependencies];
+
+	return externals;
+} 
 
 const input = process.env.INPUT_FILE; // 각 workspace의 entry
 
@@ -33,16 +41,27 @@ function getRollupConfig(input, output, format) {
 
   const config = {
     input,
-    external: ['react'],
+    external: getExternals(packageJson),
     output: [
       format === 'es' ? esOutputConfig : cjsOutputConfig,
     ],
     plugins: [
-      peerDepsExternal(), // peerDependency로 설치된 라이브러리의 코드가 번들링된 결과에 포함되지 않고, import 구문으로 불러와서 사용할 수 있게 해주는 플러그인
-      commonjs(), // cjs로 작성된 모듈을 ES6로 트랜스파일링 해주는 플러그인
+      postcss({
+        extensions: ['.scss', '.css'],
+        modules: {
+          generateScopedName: "[name]__[local]___[hash:base64:5]",
+		      hashPrefix: "prefix",
+          exportGlobals: true,
+        },
+        autoModules: false,
+        minimize: true,
+      }),
       resolve({ // Node resolve 알고리즘 을 사용하여 써드파티 모듈을 사용 가능하게 해주는 플러그인
         preferBuiltins: false,
         extensions,
+      }),
+      commonjs({
+        include: path.join(__dirname, 'node_modules/**'),
       }),
       babel({ // rollup에서 babel 을 사용 할 수 있게 해주는 플러그인
         presets: [
@@ -54,22 +73,12 @@ function getRollupConfig(input, output, format) {
           '@babel/plugin-transform-runtime',
         ],
         babelHelpers: 'runtime',
-        exclude: excludePath,
+        exclude: path.join(__dirname, 'node_modules/**'),
         extensions,
-      }),
-      postcss({
-        extensions: ['.scss', '.css'],
-        modules: {
-          generateScopedName: "[name]__[local]___[hash:base64:5]",
-		      hashPrefix: "prefix",
-          exportGlobals: true,
-        },
-        autoModules: false,
-        minimize: true,
       }),
       terser({
         maxWorkers: 4
-      })
+      }),
     ],
     preserveModules: format === 'es',
   };
@@ -78,6 +87,6 @@ function getRollupConfig(input, output, format) {
 }
 
 export default [
-  getRollupConfig(input, pkg.main, 'cjs'),
+  getRollupConfig(input, packageJson.main, 'cjs'),
   getRollupConfig(input, 'dist/esm', 'es'),
 ];
