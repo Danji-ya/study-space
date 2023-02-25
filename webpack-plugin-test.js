7f19fee36ed7c5c3853a44359ba9d1dfa11eaeaa
Danji-ya <erangerang15@gmail.com>
2023-02-25 13:34:59 +0900

    chore: repo reset

--- webpack-plugin-test.js
+++
@@ -1,9 +0,0 @@
-class WebpackPluginTest {
-    apply(compiler) {
-        compiler.hooks.done.tap('My Plugin', stats => {
-            console.log('My Plugin done');
-        })
-    }
-}
-
-module.exports = WebpackPluginTest;
