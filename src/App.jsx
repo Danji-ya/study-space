fe9ef896bee545f4925e615628cd6ec3a45cfbe6
Danji-ya <erangerang15@gmail.com>
2024-02-17 11:15:33 +0900

    feat: add postMessage feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { TEST_URL } from "./main";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  const handleOpenPopup = () => {
+    const width = 600;
+    const height = 800;
+
+    const left = window.screenX + (window.outerWidth - width) / 2;
+    const top = window.screenY + (window.outerHeight - height) / 2;
+
+    window.open(
+      TEST_URL,
+      "test",
+      `width=${width},height=${height},left=${left},top=${top}`
+    );
+  };
+
+  useEffect(() => {
+    const childResponse = (event) => {
+      if (!event?.data) return;
+      const message = event.data?.message;
+      if (!message) return;
+
+      setMessage(message);
+    };
+    window.addEventListener("message", childResponse);
+    return () => window.removeEventListener("message", childResponse);
+  }, []);
+
   return (
     <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        <h1>test</h1>
+        <div>
+          <p>{message}</p>
+          <button onClick={handleOpenPopup}>팝업 열기</button>
+        </div>
+      </main>
     </>
   );
 }
