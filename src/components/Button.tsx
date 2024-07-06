037fd79fc45d74e24ddc9aab2b1ccdb16c6793bc
Danji-ya <erangerang15@gmail.com>
2024-07-07 00:38:56 +0900

    feat(Button): css utils 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,20 @@
 import { button } from '@styled-system/recipes';
+import { css, cx } from '@styled-system/css';
 import React from 'react';
 
 const Button = () => {
-  return <button className={button({ visual: 'solid' })}>버튼</button>;
+  return (
+    <button
+      className={cx(
+        css({
+          display: 'inline-flex',
+        }),
+        button({ visual: 'solid' })
+      )}
+    >
+      버튼
+    </button>
+  );
 };
 
 export default Button;
