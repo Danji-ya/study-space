9763b9bd878defa56d3569546702f69081cac17d
Danji-ya <erangerang15@gmail.com>
2024-07-13 09:26:47 +0900

    feat: animation 및 keyframe 추가

--- src/theme/preset.ts
+++ src/theme/preset.ts
@@ -7,6 +7,42 @@ import {
 
 import { recipes } from './recipe';
 
+export const keyframes = {
+  spin: {
+    to: {
+      transform: 'rotate(360deg)',
+    },
+  },
+  ping: {
+    '75%, 100%': {
+      transform: 'scale(2)',
+      opacity: '0',
+    },
+  },
+  pulse: {
+    '50%': {
+      opacity: '.5',
+    },
+  },
+  bounce: {
+    '0%, 100%': {
+      transform: 'translateY(-25%)',
+      animationTimingFunction: 'cubic-bezier(0.8,0,1,1)',
+    },
+    '50%': {
+      transform: 'none',
+      animationTimingFunction: 'cubic-bezier(0,0,0.2,1)',
+    },
+  },
+};
+
+export const animations = {
+  spin: { value: 'spin 1s linear infinite' },
+  ping: { value: 'ping 1s cubic-bezier(0, 0, 0.2, 1) infinite' },
+  pulse: { value: 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite' },
+  bounce: { value: 'bounce 1s infinite' },
+};
+
 export const colors: Tokens['colors'] = {
   orange: {
     // DEFAULT: { value: '{colors.orange.500}' },
@@ -25,6 +61,7 @@ export const colors: Tokens['colors'] = {
 };
 
 const tokens = defineTokens({
+  animations,
   colors,
 });
 
@@ -55,6 +92,7 @@ const preset = definePreset({
     tokens,
     semanticTokens,
     recipes,
+    keyframes,
   },
   // staticCss: {
   //   recipes: {
